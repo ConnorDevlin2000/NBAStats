c8d6367bf507c61bbc6c3a55ba6632307c202931
Connor Devlin <connor@connors-air.acasmart.jh.edu>
2021-12-13 00:20:58 -0500

    Set Up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,6 @@ import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.stmt.Where;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
-import model.Employer;
-import model.Job;
 import spark.ModelAndView;
 import spark.Spark;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -22,20 +20,6 @@ import java.util.Map;
 
 public class Main {
 
-    private static Dao getEmployerORMLiteDao() throws SQLException {
-        final String URI = "jdbc:sqlite:./JBApp.db";
-        ConnectionSource connectionSource = new JdbcConnectionSource(URI);
-        TableUtils.createTableIfNotExists(connectionSource, Employer.class);
-        return DaoManager.createDao(connectionSource, Employer.class);
-    }
-
-    private static Dao getJobORMLiteDao() throws SQLException {
-        final String URI = "jdbc:sqlite:./JBApp.db";
-        ConnectionSource connectionSource = new JdbcConnectionSource(URI);
-        TableUtils.createTableIfNotExists(connectionSource, Job.class);
-        return DaoManager.createDao(connectionSource, Job.class);
-    }
-
 
     public static void main(String[] args) {
 
@@ -44,127 +28,9 @@ public class Main {
 
 
 
-        Spark.get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            if (req.cookie("username") != null)
-                model.put("username", req.cookie("username"));
-            // Solution Code starts
-            if (req.cookie("color") != null)
-                model.put("color", req.cookie("color"));
-            // Solution Code ends
-            return new ModelAndView(model, "public/index.vm");
-        }, new VelocityTemplateEngine());
-
-        Spark.post("/", (req, res) -> {
-            String username = req.queryParams("username");
-            String color = req.queryParams("color");
-            res.cookie("username", username);
-            // Solution Code starts
-            res.cookie("color", color);
-            // Solution Code ends
-            res.redirect("/");
-            return null;
-        });
-
-        Spark.get("/employers", (req, res) -> {
-            List<Employer> ls = getEmployerORMLiteDao().queryForAll();
-            Map<String, Object> model = new HashMap<String, Object>();
-            model.put("employers", ls);
-            return new ModelAndView(model, "public/employers.vm");
-        }, new VelocityTemplateEngine());
-
-        Spark.post("/employers", (req, res) -> {
-            String name = req.queryParams("name");
-            String sector = req.queryParams("sector");
-            String summary = req.queryParams("summary");
-            Employer em = new Employer(name, sector, summary);
-            getEmployerORMLiteDao().create(em);
-            res.status(201);
-            res.type("application/json");
-            return new Gson().toJson(em.toString());
-        });
-
-        Spark.get("/addemployers", (req, res) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "public/addemployers.vm");
-        }, new VelocityTemplateEngine());
-
-        // SOLUTION CODE STARTS
-        Spark.get("/jobs", (req, res) -> {
-            List<Job> ls = getJobORMLiteDao().queryForAll();
-            Map<String, Object> model = new HashMap<String, Object>();
-            model.put("jobs", ls);
-            return new ModelAndView(model, "public/jobs.vm");
-        }, new VelocityTemplateEngine());
-
-        Spark.get("/addjobs", (req, res) -> {
-            List<Employer> ls = getEmployerORMLiteDao().queryForAll();
-            Map<String, Object> model = new HashMap<String, Object>();
-            model.put("employers", ls);
-            return new ModelAndView(model, "public/addjobs.vm");
-        }, new VelocityTemplateEngine());
-
-        Spark.post("/jobs", (req, res) -> {
-            String title = req.queryParams("title");
-            Date datePosted = new SimpleDateFormat("yyyy-MM-dd").parse(req.queryParams("datePosted"));
-            Date deadline = new SimpleDateFormat("yyyy-MM-dd").parse(req.queryParams("deadline"));
-            String location = req.queryParams("location");
-            String domain = req.queryParams("domain");
-            boolean fulltime = req.queryParams("fulltime").equalsIgnoreCase("yes") ? true : false;
-            boolean salaryBased = req.queryParams("salaryBased").equalsIgnoreCase("Salary") ? true : false;
-            System.out.println(req.queryParams("payAmount"));
-            int payAmount = Integer.parseInt(req.queryParams("payAmount"));
-            String requirements = req.queryParams("requirements");
-            String employerName = req.queryParams("employer");
-            Employer employer = (Employer) getEmployerORMLiteDao().queryForEq("name", employerName).get(0);
-
-            Job job = new Job(title, datePosted, deadline, domain, location, fulltime,
-                            salaryBased, requirements, payAmount, employer);
-
-            getJobORMLiteDao().create(job);
-            res.status(201);
-            res.type("application/json");
-            return new Gson().toJson(job.toString());
-        });
-
-        Spark.get("/search", (req, res) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "public/search.vm");
-        }, new VelocityTemplateEngine());
-
-        Spark.post("/search", (req, res) -> {
-
-            String keywords = req.queryParams("keywords");
-            System.out.println(keywords);
-            String keywordsLike = "%" + keywords +"%";
-            /* Search jobs table */
-            QueryBuilder<Job, Integer> queryBuilderJobs = getJobORMLiteDao().queryBuilder();
-            Where<Job, Integer> whereJobs = queryBuilderJobs.where();
-
-            whereJobs.like("title", keywordsLike).or().like("domain", keywordsLike);
-
-            PreparedQuery<Job> preparedQueryJobs = queryBuilderJobs.prepare();
-            List<Job> jobs = getJobORMLiteDao().query(preparedQueryJobs);
-
-            /* search employers table */
-            QueryBuilder<Job, Integer> queryBuilderEmployers = getEmployerORMLiteDao().queryBuilder();
-            Where<Job, Integer> whereEmployers = queryBuilderEmployers.where();
-            whereEmployers.like("name", keywords);
-            PreparedQuery<Job> preparedQueryEmployers = queryBuilderEmployers.prepare();
-            List<Employer> employers = getEmployerORMLiteDao().query(preparedQueryEmployers);
-            if (!employers.isEmpty()) {
-                Integer id = employers.get(0).getId();
-                List jobs2 = getJobORMLiteDao().queryForEq("employer_id", id);
-                jobs.addAll(jobs2);
-            }
-
-            // set response code and return the JSON
-            res.status(200);
-            res.type("application/json");
-            return new Gson().toJson(jobs.toString());
-
-        });
-        // SOLUTION CODE ENDS
+//        Spark.get("/", (req, res) -> {
+//
+//        }, new VelocityTemplateEngine());
 
     }
 }
---
+++ src/main/java/model/Game.java
@@ -0,0 +1,86 @@
+package model;
+
+import com.j256.ormlite.field.DatabaseField;
+
+import java.util.Objects;
+
+public class Game {
+    @DatabaseField
+    private Integer gameID;
+    @DatabaseField
+    private Integer homeTeamID;
+    @DatabaseField
+    private Integer visitorTeamID;
+    @DatabaseField
+    private Integer season;
+    @DatabaseField
+    private Integer ptsHome;
+    @DatabaseField
+    private Integer fgPctHome;
+    @DatabaseField
+    private Integer ftPctHome;
+    @DatabaseField
+    private Integer fg3PctHome;
+    @DatabaseField
+    private Integer astHome;
+    @DatabaseField
+    private Integer rebHome;
+    @DatabaseField
+    private Integer ptsAway;
+    @DatabaseField
+    private Integer fgPctAway;
+    @DatabaseField
+    private Integer ftPctAway;
+    @DatabaseField
+    private Integer fg3PctAway;
+    @DatabaseField
+    private Integer astAway;
+    @DatabaseField
+    private Integer rebAway;
+
+
+
+    public Game() {
+    }
+
+    public Game(int gameID, int homeTeamID, int visitorTeamID, int season, int ptsHome, int fgPctHome, int ftPctHome, int fg3PctHome, int astHome, int rebHome, int ptsAway, int fgPctAway, int ftPctAway, int fg3PctAway, int astAway int rebAway) {
+        this.gameID = gameID;
+        this.homeTeamID = homeTeamID;
+        this.visitorTeamID = visitorTeamID;
+        this.season = season;
+        this.ptsHome = ptsHome;
+        this.fgPctHome = fgPctHome;
+        this.ftPctHome = ftPctHome;
+        this.fg3PctHome = fg3PctHome;
+        this.astHome = astHome;
+        this.rebHome = rebHome;
+        this.ptsAway = ptsHome;
+        this.fgPctAway = fgPctHome;
+        this.ftPctAway = ftPctHome;
+        this.fg3PctAway = fg3PctHome;
+        this.astAway = astHome;
+        this.rebAway = rebHome;
+    }
+
+    public Integer getGameID() {
+        return gameID;
+    }
+
+    @Override
+    public String toString() {
+        return "TBD";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game g = (Game) o;
+        return gameID.equals(g.gameID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameID);
+    }
+}
